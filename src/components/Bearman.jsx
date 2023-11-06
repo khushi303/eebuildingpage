a78eb2b63133e0783f666c7932b92c1d5209b3df
khushi303 <maankhushi303@gmail.com>
2023-11-06 10:11:15 +0530

    change in bg

--- src/components/Bearman.jsx
+++ src/components/Bearman.jsx
@@ -9,7 +9,7 @@ const Bearman = () => {
                 <h2 data-aos="fade-down" className='mb-0 ff-jakarta fw-bolder fs-xl text-white lh-120 text-center py-4'>Amplify and <span className='text-uppercase'>stretch gains</span></h2>
             </Container>
             <div className='bg-bear d-flex align-items-center justify-content-md-center justify-content-end flex-column position-relative'>
-                <div className='bg_black1'>
+                <div className='bg_black1 w-100'>
                     <Container className='custom_container d-flex justify-content-center pb-md-0 mb-md-0 pt-md-0 pb-sm-5 pb-3 mb-sm-0 mb-3 pt-4'>
                         <p data-aos="fade-up" className='mb-0 ff-raleway fs-sm fw-normal text-white lh-160 mw-554 text-center'>We take a different balanced approach to finding and creating yield opportunities in the everchanging crypto market. Our yield strategies help users stay protected and hedged during bear markets and amplify yield during bull markets without resorting to leverage or other dangerous techniques.</p>
                     </Container>
