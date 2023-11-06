fce6d215e8c02d19b8c2007329da64a8cdca1206
khushi303 <maankhushi303@gmail.com>
2023-11-06 11:15:25 +0530

    change in form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { useState } from 'react';
 
 const Contact = () => {
+    const [name, setname] = useState("");
+    const contactchange = (e) => {
+        setname(e.target.value);
+        console.log(e.target.value);
+    }
     return (
         <div className='bg_contactimg contact_pad'>
             <Container className='custom_container d-flex align-items-center justify-content-center flex-column'>
@@ -9,7 +15,7 @@ const Contact = () => {
                 <p data-aos="fade-right" className='ff-raleway fs-2sm fw-normal text-center text-white lh-160 op_08'>Have a question or want to contribute? Reach out to us.</p>
                 <form data-aos="fade-left" className='pt-4 pb-4 w-100'>
                     <div className='w-100 d-flex gap-5 pb-3'>
-                        <input type="text" placeholder="First Name" className='w-50 py-4 bg-transparent outline_none border_bottom ff-raleway fs-2sm fw-normal text-white lh-150' />
+                        <input type="text" value={name} onChange={contactchange} placeholder="First Name" className='w-50 py-4 bg-transparent outline_none border_bottom ff-raleway fs-2sm fw-normal text-white lh-150' />
                         <input type="text" placeholder="Last Name" className='w-50 py-4 bg-transparent outline_none border_bottom ff-raleway fs-2sm fw-normal text-white lh-150' />
                     </div>
                     <div className='w-100 d-flex gap-5 pb-3'>
